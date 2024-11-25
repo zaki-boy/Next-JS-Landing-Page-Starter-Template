926b0eafdf0e320b500e735404286cb888791a7a
unknown <rahmadfauzi600@gmail.com>
2024-11-25 22:43:05 +0700

    update coverage test

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
--- utils/multiplier.ts
+++ utils/multiplier.ts
@@ -1,3 +1,11 @@
 export function multiplier(a: number, b: number): number {
+  if (a === Infinity || b === Infinity) {
+    return Infinity; // This behavior is not tested
+  }
+
+  if (a === 0 || b === 0) {
+    return 0;
+  }
+
   return a * b;
 }
