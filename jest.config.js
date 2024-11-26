2b96487a58b3b19d08e6aa31e2995a3378d63ff0
Agung Muzaki <70474093+zaki-boy@users.noreply.github.com>
2024-11-26 10:56:42 +0700

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,10 @@ module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
     },
   },
 };
